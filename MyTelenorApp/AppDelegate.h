6d6975614bdff7d0fcefc5587c7aab666d68a1e7
Muhammad Irfan Awan <irfan.awaan@gmail.com>
2015-11-17 10:09:14 +0500

    Initial Commit

---
+++ MyTelenorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTelenorApp
+//
+//  Created by Muhammad Irfan on 11/17/15.
+//  Copyright © 2015 Muhammad Irfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTelenorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTelenorApp
+//
+//  Created by Muhammad Irfan on 11/17/15.
+//  Copyright © 2015 Muhammad Irfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
